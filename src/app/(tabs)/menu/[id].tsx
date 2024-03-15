eaa8dcc3a9f6a8fbb557ab7e371885ac33758181
Jose Salazar <jose.salazar@blueriver.com>
2024-03-15 13:25:54 -0500

    Shopping Cart

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,21 +1,30 @@
-import { useLocalSearchParams, Stack } from 'expo-router';
+import { useLocalSearchParams, Stack, useRouter } from 'expo-router';
 import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
 import products from '@/assets/data/products';
 import { defaultPizzaImage } from '@/src/components/ProductListItem';
 import { useState } from 'react';
 import Button from '@components/Button';
+import { useCart } from '@/src/providers/CartProvider';
+import { PizzaSize } from '@/src/types';
 
-const sizes = ['S', 'M', 'L', 'XL']
+const sizes: PizzaSize[] = ['S', 'M', 'L', 'XL']
 
 const ProductDetailsScreen = () => {
     const { id } = useLocalSearchParams();
+    const { addItem } = useCart();
 
-    const [selectedSize, setSelectedSize] = useState('M');
+    const router = useRouter();
+
+    const [selectedSize, setSelectedSize] = useState<PizzaSize>('M');
 
     const product = products.find((p) => p.id.toString() === id);
 
     const addToCart = () => {
-        console.warn('Add to cart, product:', product?.name, 'size:', selectedSize);
+        if (!product) {
+            return;
+        }
+        addItem(product, selectedSize)
+        router.push('/cart')
     }
 
     if (!product) {
