e78c20efc664ab1c6f0b0dc53d8fa2ae4b4d6b05
Jose Salazar <jose.salazar@blueriver.com>
2024-03-15 16:05:53 -0500

    Admin Side

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,7 +1,7 @@
 import Colors from '@/src/constants/Colors';
 import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import { Product } from '../types';
-import { Link } from 'expo-router';
+import { Link, useSegments } from 'expo-router';
 
 export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
 
@@ -12,8 +12,14 @@ type ProductListItemProps = {
 
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
+    const segments = useSegments()
+
+    if (!segments[0]) {
+        return 'admin';
+    }
+
     return (
-        <Link href={`/menu/${product.id}`} asChild>
+        <Link href={`/${segments[0]}/menu/${product.id}`} asChild>
             <Pressable style={styles.container}>
                 <Image
                     source={{ uri: product.image || defaultPizzaImage }}
