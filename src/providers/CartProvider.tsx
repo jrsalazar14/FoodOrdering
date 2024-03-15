b160245a0317245b780f462f01b61fdd1a751879
Jose Salazar <jose.salazar@blueriver.com>
2024-03-15 13:31:16 -0500

    add total

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -6,12 +6,14 @@ type CartType = {
     items: CartItem[];
     addItem: (product: Product, size: CartItem['size']) => void;
     updateQuantity: (itemId: string, amount: -1 | 1) => void;
+    total: number;
 }
 
 const CartContext = createContext<CartType>({
     items: [],
     addItem: () => { },
-    updateQuantity: () => { }
+    updateQuantity: () => { },
+    total: 0,
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
@@ -43,9 +45,10 @@ const CartProvider = ({ children }: PropsWithChildren) => {
         )
     }
 
+    const total = items.reduce((sum, item) => (sum + item.product.price * item.quantity), 0);
 
     return (
-        <CartContext.Provider value={{ items, addItem, updateQuantity }}>
+        <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
             {children}
         </CartContext.Provider>
     )
