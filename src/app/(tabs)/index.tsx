73f445a97417b37965e74323bd1fd27e2a461ec7
Jose Salazar <jose.salazar@blueriver.com>
2024-03-15 09:00:48 -0500

    Expo Router

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,17 +1,5 @@
-import { FlatList, View } from 'react-native';
-import products from '@/assets/data/products';
-import ProductListItem from '@components/ProductListItem';
+import { Redirect } from "expo-router";
 
-export default function MenuScreen() {
-  return (
-    <View>
-      <FlatList
-        data={products}
-        renderItem={({ item }) => <ProductListItem product={item} />}
-        numColumns={2}
-        contentContainerStyle={{ gap: 10, padding: 10 }}
-        columnWrapperStyle={{ gap: 10 }}
-      />
-    </View>
-  );
+export default function TabIndex() {
+  return <Redirect href={'/(tabs)/menu/'} />;
 }
---
+++ src/app/(tabs)/menu/[id].tsx
@@ -0,0 +1,15 @@
+import { useLocalSearchParams, Stack } from 'expo-router';
+import { View, Text } from 'react-native';
+
+const ProductDetailsScreen = () => {
+    const { id } = useLocalSearchParams();
+
+    return (
+        <View>
+            <Stack.Screen options={{ title: 'Product Details' }} />
+            <Text>ProductDetailsScreen for id: {id}</Text>
+        </View>
+    );
+}
+
+export default ProductDetailsScreen;
