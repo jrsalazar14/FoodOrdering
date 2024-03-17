c967c4be803bf8c0013178fbdaee931e0c33a05d
Jose Salazar <jose.salazar@blueriver.com>
2024-03-16 19:19:33 -0500

    Add Order Status

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -10,6 +10,8 @@ import {
 import OrderListItem from "@/src/components/OrderListItem";
 import orders from "@/assets/data/orders";
 import OrderItemListItem from "@/src/components/OrderItemListItem";
+import Colors from "@/src/constants/Colors";
+import { OrderStatusList } from "@/src/types";
 
 const OrderDetailsScreen = () => {
   const { id } = useLocalSearchParams();
@@ -30,6 +32,39 @@ const OrderDetailsScreen = () => {
         data={order.order_items}
         renderItem={({ item }) => <OrderItemListItem item={item} />}
         contentContainerStyle={{ gap: 10 }}
+        ListFooterComponent={() => (
+          <>
+            <Text style={{ fontWeight: "bold" }}>Status</Text>
+            <View style={{ flexDirection: "row", gap: 5 }}>
+              {OrderStatusList.map((status) => (
+                <Pressable
+                  key={status}
+                  onPress={() => console.warn("Update status")}
+                  style={{
+                    borderColor: Colors.light.tint,
+                    borderWidth: 1,
+                    padding: 10,
+                    borderRadius: 5,
+                    marginVertical: 10,
+                    backgroundColor:
+                      order.status === status
+                        ? Colors.light.tint
+                        : "transparent",
+                  }}
+                >
+                  <Text
+                    style={{
+                      color:
+                        order.status === status ? "white" : Colors.light.tint,
+                    }}
+                  >
+                    {status}
+                  </Text>
+                </Pressable>
+              ))}
+            </View>
+          </>
+        )}
       />
     </View>
   );
