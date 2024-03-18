7a9460a0930be363f7eb64d239c4f4a646084183
Jose Salazar <jose.salazar@blueriver.com>
2024-03-18 18:05:47 -0500

    supabase types

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -1,58 +1,67 @@
 import { PropsWithChildren, createContext, useContext, useState } from "react";
-import { CartItem, Product } from "../types";
-import { randomUUID } from 'expo-crypto'
+import { CartItem, Tables } from "../types";
+import { randomUUID } from "expo-crypto";
 
 type CartType = {
-    items: CartItem[];
-    addItem: (product: Product, size: CartItem['size']) => void;
-    updateQuantity: (itemId: string, amount: -1 | 1) => void;
-    total: number;
-}
+  items: CartItem[];
+  addItem: (product: Tables<"products">, size: CartItem["size"]) => void;
+  updateQuantity: (itemId: string, amount: -1 | 1) => void;
+  total: number;
+};
 
 const CartContext = createContext<CartType>({
-    items: [],
-    addItem: () => { },
-    updateQuantity: () => { },
-    total: 0,
+  items: [],
+  addItem: () => {},
+  updateQuantity: () => {},
+  total: 0,
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
-    const [items, setItems] = useState<CartItem[]>([]);
+  const [items, setItems] = useState<CartItem[]>([]);
 
-    const addItem = (product: Product, size: CartItem['size']) => {
-        const existingItem = items.find(item => item.product === product && item.size === size);
+  const addItem = (product: Tables<"products">, size: CartItem["size"]) => {
+    const existingItem = items.find(
+      (item) => item.product === product && item.size === size
+    );
 
-        if (existingItem) {
-            return updateQuantity(existingItem.id, 1);
-        }
-
-        const newCartItem: CartItem = {
-            id: randomUUID(),
-            product,
-            product_id: product.id,
-            size,
-            quantity: 1
-        }
-
-        setItems([newCartItem, ...items]);
+    if (existingItem) {
+      return updateQuantity(existingItem.id, 1);
     }
 
-    const updateQuantity = (itemId: string, amount: -1 | 1) => {
-        setItems(items.map(item =>
-            item.id !== itemId
-                ? item
-                : { ...item, quantity: item.quantity + amount }).filter((item) => item.quantity > 0)
+    const newCartItem: CartItem = {
+      id: randomUUID(),
+      product,
+      product_id: product.id,
+      size,
+      quantity: 1,
+    };
+
+    setItems([newCartItem, ...items]);
+  };
+
+  const updateQuantity = (itemId: string, amount: -1 | 1) => {
+    setItems(
+      items
+        .map((item) =>
+          item.id !== itemId
+            ? item
+            : { ...item, quantity: item.quantity + amount }
         )
-    }
+        .filter((item) => item.quantity > 0)
+    );
+  };
 
-    const total = items.reduce((sum, item) => (sum + item.product.price * item.quantity), 0);
+  const total = items.reduce(
+    (sum, item) => sum + item.product.price * item.quantity,
+    0
+  );
 
-    return (
-        <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
-            {children}
-        </CartContext.Provider>
-    )
-}
+  return (
+    <CartContext.Provider value={{ items, addItem, updateQuantity, total }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
 
 export default CartProvider;
 
